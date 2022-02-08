7137f32b251077241c86c84e736674059080ea4f
dmonfan <37087706+dmonfan@users.noreply.github.com>
2022-02-08 01:14:47 -0700

    Added class

--- generate_multiplication_problem.py
+++ generate_multiplication_problem.py
@@ -1,6 +1,19 @@
 from random import randint
-x = randint(100,999)
-y = randint(100,999)
-print("The numbers to multiply are: " + str(x) + " & " + str(y))
-z = x * y
-print("They equal " + str(z) + ".")
+
+class multiplication_problem:
+
+	def __init__(self):
+		self.x = randint(100,999)
+		self.y = randint(100,999)
+		self.z = self.x * self.y
+
+	def f(self):
+		return "   " + '{:>3}'.format(self.x) + "\nX  " + '{:>3}'.format(self.y)
+
+	def a(self):
+		return '{:>6}'.format(self.z)
+
+if __name__ == '__main__':
+	problem = multiplication_problem()
+	print(problem.f())
+	print(problem.a())
