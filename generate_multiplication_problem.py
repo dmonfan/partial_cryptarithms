3c7ecbf8345c50cd71c3de4a95932d7d24e8518c
dmonfan <37087706+dmonfan@users.noreply.github.com>
2022-02-12 16:28:02 -0700

    Added var size

--- generate_multiplication_problem.py
+++ generate_multiplication_problem.py
@@ -1,10 +1,13 @@
 from random import randint
 
-class multiplication_problem:
+class multiplication_problem():
 
-	def __init__(self):
-		self.x = randint(100,999)
-		self.y = randint(100,999)
+	def __init__(self, size):
+		self.size = size
+		lower = pow(10, self.size - 1)
+		upper = pow(10, self.size) - 1
+		self.x = randint(lower, upper)
+		self.y = randint(lower, upper)
 		self.z = self.x * self.y
 
 	def f(self):
@@ -14,6 +17,10 @@ class multiplication_problem:
 		return '{:>6}'.format(self.z)
 
 if __name__ == '__main__':
-	problem = multiplication_problem()
+	problem = multiplication_problem(3)
+	print(problem.f())
+	print(problem.a())
+
+	problem = multiplication_problem(2)
 	print(problem.f())
 	print(problem.a())
