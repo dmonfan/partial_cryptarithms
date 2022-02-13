75160170bbf32d4c06f61cd738d765acd5c0828d
dmonfan <37087706+dmonfan@users.noreply.github.com>
2022-02-12 22:05:19 -0700

    Added replayability

--- partial_cryptarithms.py
+++ partial_cryptarithms.py
@@ -16,67 +16,73 @@ def difficulty_to_subs(num):
 	subs = int(i / 4) + 1
 	return subs
 
-notset = True
-while notset:
-	difficulty = input("Set difficulty (0-9): ")
-	if(str_is_digit(difficulty) == True):
-		notset = False
-	else:
-		print("Type only a digit 0-9.")
+replay = True
+while(replay):
 
-difficulty = int(difficulty)
-digits = difficulty_to_digits(difficulty)
-subs = difficulty_to_subs(difficulty)
+	notset = True
+	while notset:
+		difficulty = input("Set difficulty (0-9): ")
+		if(str_is_digit(difficulty) == True):
+			notset = False
+		else:
+			print("Type only a digit 0-9.")
 
-problem = multiplication_problem(digits)
-digits1 = unique_digits(problem.x)
-digits2 = unique_digits(problem.y)
+	difficulty = int(difficulty)
+	digits = difficulty_to_digits(difficulty)
+	subs = difficulty_to_subs(difficulty)
 
-digits12 = digits1.union(digits2)
+	problem = multiplication_problem(digits)
+	digits1 = unique_digits(problem.x)
+	digits2 = unique_digits(problem.y)
 
-sub_dict = {}
+	digits12 = digits1.union(digits2)
 
-if (subs >= len(digits12)):
-	subs = len(digits12)
+	sub_dict = {}
 
-letters = {"a", "b", "c", "d"}
+	if (subs >= len(digits12)):
+		subs = len(digits12)
 
-for sub in range(subs):
-	sub_dict[digits12.pop()] = letters.pop()
+	letters = {"a", "b", "c", "d"}
 
-str_problem_x = str(problem.x)
-str_problem_y = str(problem.y)
-str_problem_z = str(problem.z)
+	for sub in range(subs):
+		sub_dict[digits12.pop()] = letters.pop()
 
-rep_str_problems = []
+	str_problem_x = str(problem.x)
+	str_problem_y = str(problem.y)
+	str_problem_z = str(problem.z)
 
-str_problems = [str_problem_x, str_problem_y, str_problem_z]
-for str_problem in str_problems:
+	rep_str_problems = []
+
+	str_problems = [str_problem_x, str_problem_y, str_problem_z]
+	for str_problem in str_problems:
+		for k, v in sub_dict.items():
+			str_problem = str_problem.replace(k, v)
+		rep_str_problems.append(str_problem)
+
+	partial_cryptarithm = multiplication_cryptarithm_problem_container(rep_str_problems[0],rep_str_problems[1],rep_str_problems[2], digits)
+
+	print()
+
+	for v in sub_dict.values():
+		print(v + " = ?")
+		
+	print()
+
+	print(partial_cryptarithm.f())
+	print(partial_cryptarithm.a())
+
+	print()
+
+	input("Press return to reveal answer: \n")
+
 	for k, v in sub_dict.items():
-		str_problem = str_problem.replace(k, v)
-	rep_str_problems.append(str_problem)
+		print(v + " = " + k)
+	print()
 
-partial_cryptarithm = multiplication_cryptarithm_problem_container(rep_str_problems[0],rep_str_problems[1],rep_str_problems[2], digits)
+	print(problem.f())
+	print(problem.a())
 
-print()
+	print()
 
-for v in sub_dict.values():
-	print(v + " = ?")
-	
-print()
-
-print(partial_cryptarithm.f())
-print(partial_cryptarithm.a())
-
-print()
-
-input("Press return to reveal answer: \n")
-
-for k, v in sub_dict.items():
-	print(v + " = " + k)
-print()
-
-print(problem.f())
-print(problem.a())
-
-print()
+	if(input("Press q to quit or any key to replay: ") == 'q'):
+		replay = False
